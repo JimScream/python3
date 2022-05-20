0c8490ce1bc3bfb55109ae467a9b9a825c565439
jimscream <szymon.wrzeszcz@gmail.com>
2022-05-21 01:07:06 +0200

    Python na start

---
+++ python na start.py
@@ -0,0 +1,21 @@
+# x = 2.0
+# y = 3.2
+# z = 33j
+
+imie = "Simon"
+drugieimie = "Mateusz"
+nickname = "JimScream"
+
+# print ("wynik dla x + y =", x + y)
+
+caleimie = imie + drugieimie
+przerwa = " "
+print("Imię: " + imie + przerwa + nickname)
+
+
+celsjusze = int(input("Podaj temperaturę w stopniach Celsjusza: "))
+print("farenheit: ")
+print(celsjusze*9/5 +32)
+
+for i in range(1):
+    print(i)
